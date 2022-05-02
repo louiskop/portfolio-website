ee8b0639d5888360b749ac5d14beebde6be519e1
Louis de Jager <louisdj12@gmail.com>
2022-05-02 22:29:13 +0200

    Footer Game Prototype

--- public/game.js
+++ public/game.js
@@ -7,7 +7,18 @@ const c = canvas.getContext('2d');
 
 // change canvas size
 canvas.width = window.innerWidth;
-canvas.height = 100;
+canvas.height = 300;
+
+// keys to monitor for input
+let keys = {
+    right: false,
+    left: false,
+    up: false,
+    down: false,
+};
+let jumpCount = 0;
+let alreadyJumping = false;
+let platformStand = true;
 
 // player class
 class Player {
@@ -20,12 +31,12 @@ class Player {
         this.height = 50;
             
         this.position = {
-            x : 500,
-            y : 100
+            x : 20,
+            y : 20
         }
 
         this.velocity = {
-            x : -3,
+            x : 0,
             y : 0,
         }
 
@@ -34,9 +45,7 @@ class Player {
     // draw the player on the canvas
     draw(){
         c.fillStyle = '#4287f5' 
-        c.rect(10, 20, 20, 20);
-        c.fill();
-        
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);        
     }
 
     // update the player on the canvas
@@ -44,32 +53,170 @@ class Player {
         this.draw();
 
         // update position
-        this.position.x += this.position.x;
-        this.position.y += this.position.y;
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
 
     }
 
 }
 
-// create player and obstacles
-const player = new Player(4);
-player.update();
+// platform class 
+class Platform {
 
-// main game loop
-function loop(){
-    requestAnimationFrame(loop);
+    constructor(x, width){
+    
+        this.width = width;
+        this.height = 20;
 
-    // clear canvas
-    c.clearRect(0,0,canvas.width, canvas.height);
+        this.position = {
+            x : x,
+            y : 280,
+        }
+    }
 
-    // update player
-    player.update();
+    draw(){
+        c.fillStyle = '#FF0000' 
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
 
-    // check for socials
 
 }
 
+// create player and platforms
+const player = new Player(4);
+player.update();
+const platforms = [new Platform(0, 200), new Platform(300, 50)];
+platforms.forEach((platform) => {platform.draw()});
+
+function platformCollision(){
+
+    platformStand = false;
+
+    platforms.forEach((platform) => {
+
+        if(platformStand){
+            return;
+        }
+
+        if (player.position.y + player.height >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width ){
+            // player is standing on a platform
+            platformStand = true;
+            player.velocity.y = 0;
+        }else {
+            platformStand = false;
+            player.velocity.y = 7;
+        }
+    });
+}
 
 
-// Unit tests 
-loop();
+// main game loop
+function animate(){
+    
+    // make this the game loop
+    requestAnimationFrame(animate);
+
+    // clear the canvas
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    // update player and platforms
+    player.update();
+    platforms.forEach((platform) => {platform.draw()});
+
+    // collision checking and gravity
+    platformCollision();
+
+    // handle controls
+    // jump
+    let canJump = alreadyJumping || keys.up && platformStand;
+    if(canJump){
+    
+        // check if the player is currently jumping
+        alreadyJumping = true;
+
+        // increase velocity for jump based on duration
+        if(jumpCount < 5){
+            player.velocity.y = -15;
+        }else if(jumpCount < 10){
+            player.velocity.y = -10
+        }
+        else{
+            player.velocity.y = -5;
+        }
+        
+
+        // time how long the player jumps
+        jumpCount++;
+
+        // stop jump after 30 frames
+        if(jumpCount > 20){
+            player.velocity.y = 0;
+            jumpCount = 0
+            keys.up = false;
+            canJump = false;
+            alreadyJumping = false;
+        }
+
+
+    }
+
+    // move right
+    if(keys.right){
+        player.velocity.x = 7;     
+    }
+    // move left
+    else if(keys.left){
+        player.velocity.x = -7;
+    }else {
+        player.velocity.x = 0;
+    }
+
+    // // check for socials
+    // if(keys.down){
+    //     // check if on social and visit that social
+    // }
+
+
+    // check if player is out of bounds (respawn)
+
+}
+
+// Unit tests and function run
+animate();
+
+// event listeners 
+window.addEventListener('keydown', ({keyCode}) => {
+
+    switch(keyCode){
+
+        // move right (39)
+        case 39: keys.right = true; break;
+
+        // move left (37)
+        case 37: keys.left = true; break;
+
+        // jump (38)
+        case 38: keys.up = true; break;
+
+        // down (40)
+        case 40: keys.down = true; break;
+
+    } 
+})
+
+window.addEventListener('keyup', ({keyCode}) => {
+
+    switch(keyCode){
+
+        // move right (39)
+        case 39: keys.right = false; break;
+
+        // move left (37)
+        case 37: keys.left = false; break;
+
+        // jump (38)
+        case 38: keys.up = false; break;
+
+    } 
+})
+
